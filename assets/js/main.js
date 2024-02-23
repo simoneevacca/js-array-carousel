1fbdffed829346d0f9cb365ff2c650fe08d1668f
simoneevacca <v.simone.vacca@gmail.com>
2024-02-23 15:00:38 +0100

    add variable

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const image = [
+    '01.webp',
+    '02.webp',
+    '03.webp',
+    '04.webp',
+    '05.webp',
+]
+
+const visibleImage = 0;
+
+const slides = document.querySelector('.carosel');
+const next = document.querySelector('.next');
+const previus = document.querySelector('.previus');
+
+console.log(image, visibleImage, slides);
+
